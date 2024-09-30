04e33b4e231f6235065bc6deeae6ed49d5a4fafd
Suhan Wijaya <suhanw@gmail.com>
2024-09-30 19:57:59 +0800

    Fetch data with `axios`

--- client/src/components/DataComponent.test.tsx
+++ client/src/components/DataComponent.test.tsx
@@ -3,9 +3,13 @@ import { render, screen } from "@testing-library/react";
 import DataComponent from "./DataComponent";
 
 describe("DataComponent", () => {
-  it("should fetch tasks", () => {
+  it("should fetch tasks", async () => {
     render(<DataComponent />);
 
     expect(screen.getByText("This is DataComponent.")).toBeInTheDocument();
+
+    // wait until the `get` request promise resolves 
+    await screen.findByText('"Yeah." -John Wick');
+    expect(screen.getByText('"Yeah." -John Wick')).toBeInTheDocument();
   });
 });
--- client/src/components/DataComponent.tsx
+++ client/src/components/DataComponent.tsx
@@ -1,13 +1,68 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
+import axios from "axios";
+
+interface State {
+  error?: string | null;
+  quote?: string | null;
+}
+
+interface Action extends State {
+  type: string;
+}
+
+const initialState: State = {
+  error: null,
+  quote: null,
+};
+
+function quoteReducer(state: State, action: Action): State {
+  switch (action.type) {
+    case "SUCCESS": {
+      return {
+        error: null,
+        quote: action.quote,
+      };
+    }
+    case "ERROR": {
+      return {
+        error: action.error,
+        quote: null,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+}
 
 function DataComponent() {
+  const [{ error, quote }, dispatch] = useReducer(quoteReducer, initialState);
+
   useEffect(() => {
-    fetch("/user")
-      .then((res) => res.json())
-      .then((data) => console.log({ data }));
+    axios
+      .get("/user")
+      .then(({ data }) => {
+        dispatch({
+          type: "SUCCESS",
+          quote: `"Yeah." -${data.firstName} ${data.lastName}`,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+        dispatch({
+          type: "ERROR",
+          error: "Uh oh. Something went wrong.",
+        });
+      });
   }, []);
 
-  return <div>This is DataComponent.</div>;
+  return (
+    <div>
+      <p>This is DataComponent.</p>
+      {quote && <p>{quote}</p>}
+      {error && <p>{error}</p>}
+    </div>
+  );
 }
 
 export default DataComponent;
